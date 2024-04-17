a5f5acce4e41650d96942ad47fd098d962cd79a2
Anup <anuup22@outlook.com>
2024-04-17 22:31:04 +0530

    added featured + tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/1 antialiased bg-grid-white/[0.02]">
       
       <HeroSection />
+      <FeaturedCourses />
     </main>
     
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ function Navbar({ className }: { className?: string }) {
 
   return (
     <div
-    className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 border-[1.5px] border-green-300 rounded-[35px]", className)}
+    className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 rounded-[35px]", className)} 
     >
         <Menu setActive={setActive}>
             <Link href={"/"}>
